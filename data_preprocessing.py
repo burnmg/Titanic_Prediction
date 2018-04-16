48265e1e073ca548878893d55b3eb2a2ecdd78d9
burnmg <rlin1992@hotmail.com>
2018-04-16 15:34:27 +0800

    1

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib as mat
+import seaborn as sb
 
 mat.interactive(False)
 
@@ -24,3 +25,19 @@ train.loc[j,'Age'] = train['Age'][train['Pclass']==2].mean()
 j = (train['Age'].isna()) & (train['Pclass'] == 3)
 train.loc[j,'Age'] = train['Age'][train['Pclass']==3].mean()
 
+train.info()
+## clean rest of missing value
+train.dropna(inplace = True)
+train.info()
+
+# Create dummy variables
+embarked_dummy = pd.get_dummies(train['Embarked'], drop_first=True)
+sex_dummy = pd.get_dummies(train['Sex'], drop_first=True)
+train.drop(['Embarked', 'Sex'], axis = 1, inplace = True)
+train = pd.concat([train, sex_dummy, embarked_dummy],axis = 1)
+
+# check correlation between variables
+sb.heatmap(train.corr(),vmin=-1, vmax=1, cmap='Spectral')
+
+# we found that Fare is correlated with Pclass, we drop one of these
+train.drop(['Fare'], inplace = True, axis = 1)
--- logistic_regression.py
+++ logistic_regression.py
@@ -1,5 +1,3 @@
 import data_preprocessing
 import sklearn as sk
 import seaborn as sb
-
-sk.
